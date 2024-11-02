075c5a89c383ec604636dcdeeb10c1155348c010
Nabin Thapa <erewhonaden23@gmail.com>
2024-11-02 14:54:29 +0545

    Update: added table from bubbletea

---
+++ constants/constants.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+	LOCATION               = "/sys/bus/platform/devices/VPC2004:00/"
+	CONSERVATION_MODE_FILE = "/sys/bus/platform/devices/VPC2004:00/conservation_mode"
+	FN_LOCK                = "/sys/bus/platform/devices/VPC2004:00/fn_lock"
+	USB_CHARGING           = "/sys/bus/platform/devices/VPC2004:00/usb_charging"
+)
--- main.go
+++ main.go
@@ -6,59 +6,77 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
+
+	"github.com/nabinthapaa/vantage/constants"
+	"github.com/nabinthapaa/vantage/table"
 )
 
-var CONSERVATION_MODE_FILE = "/sys/bus/platform/devices/VPC2004:00/conservation_mode"
-
 func main() {
+	user := os.Getenv("USER")
+
 	if len(os.Args) < 2 {
-		fmt.Println("Please provide 'on' or 'off' as a flag.")
+		log.Fatalf("Please provide 'on' 'off' or 'table' as a flag.")
 		os.Exit(1)
 	}
+
+	if user != "root" {
+		log.Fatalf("Permission denied. Run sudo %s", strings.Join(os.Args, " "))
+		os.Exit(1)
+	}
+
 	flag := os.Args[1]
 
-	file, err := os.OpenFile(CONSERVATION_MODE_FILE, os.O_RDWR, 0644)
-	if err != nil {
-		log.Fatalf("Failed to open conservation_mode file: %v", err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	if !scanner.Scan() {
-		log.Fatal("Failed to read from conservation_mode file")
-	}
-	fileContent := scanner.Text()
-
-	mode, err := strconv.Atoi(fileContent)
-	if err != nil {
-		log.Fatalf("Error parsing mode: %v", err)
-	}
-
-	switch flag {
-	case "on":
-		if mode == 1 {
-			fmt.Println("Conservation mode is already on.")
-			return
+	if flag == "table" {
+		if err := table.Run(); err != nil {
+			fmt.Printf("err %v", err)
+			os.Exit(1)
 		}
-		fmt.Println("Turning on conservation mode...")
-		if _, err := file.WriteString("1"); err != nil {
-			log.Fatalf("Failed to write to conservation_mode file: %v", err)
+	} else {
+		file, err := os.OpenFile(constants.CONSERVATION_MODE_FILE, os.O_RDWR, 0644)
+		if err != nil {
+			log.Fatalf("Failed to open conservation_mode file: %v", err)
 		}
-		fmt.Println("Conservation mode enabled.")
+		defer file.Close()
 
-	case "off":
-		if mode == 0 {
-			fmt.Println("Conservation mode is already off.")
-			return
+		scanner := bufio.NewScanner(file)
+		if !scanner.Scan() {
+			log.Fatal("Failed to read from conservation_mode file")
 		}
-		fmt.Println("Turning off conservation mode...")
-		if _, err := file.WriteString("0"); err != nil {
-			log.Fatalf("Failed to write to conservation_mode file: %v", err)
-		}
-		fmt.Println("Conservation mode disabled.")
+		fileContent := scanner.Text()
 
-	default:
-		fmt.Println("Invalid flag. Use 'on' or 'off'.")
-		os.Exit(1)
+		mode, err := strconv.Atoi(fileContent)
+		if err != nil {
+			log.Fatalf("Error parsing mode: %v", err)
+		}
+
+		switch flag {
+		case "on":
+			if mode == 1 {
+				fmt.Println("Conservation mode is already on.")
+				return
+			}
+			fmt.Println("Turning on conservation mode...")
+			if _, err := file.WriteString("1"); err != nil {
+				log.Fatalf("Failed to write to conservation_mode file: %v", err)
+			}
+			fmt.Println("Conservation mode enabled.")
+
+		case "off":
+			if mode == 0 {
+				fmt.Println("Conservation mode is already off.")
+				return
+			}
+			fmt.Println("Turning off conservation mode...")
+			if _, err := file.WriteString("0"); err != nil {
+				log.Fatalf("Failed to write to conservation_mode file: %v", err)
+			}
+			fmt.Println("Conservation mode disabled.")
+
+		default:
+			fmt.Println("Invalid flag. Use 'on' or 'off'.")
+			os.Exit(1)
+		}
+
 	}
 }
